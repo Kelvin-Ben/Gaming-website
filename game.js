064c83a718b3ade70df1a5b836b6f81e4a03472c
Kelvin-Ben <kevoben14@gmail.com>
2023-05-27 12:22:19 +0300

    Update the fies

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
-const navToggle = document.querySelector('#nav_toggle')
+const navToggle = document.querySelector('#nav_toggle');
 const sideMenu = document.querySelector('#nav_menu-container');
 // const overlay = document.querySelector('.overlay')
-const heroMarketingText = document.querySelector('.hero_marketing-text')
+const heroMarketingText = document.querySelector('.hero_marketing-text');
 navToggle.addEventListener('click', () => {
-  sideMenu.classList.toggle('show')
-  heroMarketingText.classList.toggle('marketing-text')
-  navToggle.classList.toggle('close')
+  sideMenu.classList.toggle('show');
+  heroMarketingText.classList.toggle('marketing-text');
+  navToggle.classList.toggle('close');
 });
