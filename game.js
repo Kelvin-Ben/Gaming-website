c31bf3a8462ea2c911566602582f3fb2b81bb889
Kelvin-Ben <kevoben14@gmail.com>
2023-06-06 22:51:39 +0300

    Modified the code

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
-const navToggle = document.querySelector('#nav_toggle');
-const sideMenu = document.querySelector('#nav_menu-container');
+const navToggle = document.querySelector("#nav_toggle");
+const sideMenu = document.querySelector("#nav_menu-container");
 // const overlay = document.querySelector('.overlay')
-const heroMarketingText = document.querySelector('.hero_marketing-text');
-navToggle.addEventListener('click', () => {
-  sideMenu.classList.toggle('show');
-  heroMarketingText.classList.toggle('marketing-text');
-  navToggle.classList.toggle('close');
+const heroMarketingText = document.querySelector(".hero_marketing-text");
+navToggle.addEventListener("click", () => {
+  sideMenu.classList.toggle("show");
+  heroMarketingText.classList.toggle("marketing-text");
+  navToggle.classList.toggle("close");
 });
